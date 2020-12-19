910bacff6ec7a823e8b2c635c4d2b24e014a0d2b
dpernus <dpernus@gmail.com>
2020-12-19 12:00:49 -0300

    Add todo list component and events to add and delete todo

---
+++ src/app/components/todo-list/todo-list.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/interfaces/todo';
+
+@Component({
+  selector: 'todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements OnInit {
+  todos: Todo[] = [];
+  todoTitle: string = '';
+  idForTodo: number = 0;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.idForTodo = 4
+    this.todoTitle = '';
+    this.todos = [
+      {
+        'id': 1,
+        'title': 'Finish Angular Screencast',
+        'completed': false,
+        'editing': false,
+      },
+      {
+        'id': 2,
+        'title': 'Take over world',
+        'completed': false,
+        'editing': false,
+      },
+      {
+        'id': 3,
+        'title': 'One more thing',
+        'completed': false,
+        'editing': false,
+      },
+    ];
+  }
+
+  addTodo(): void {
+    if (this.todoTitle.trim().length === 0) {
+      return
+    }
+
+    this.todos.push({
+      id: this.idForTodo,
+      title: this.todoTitle,
+      completed: false,
+      editing: false
+    })
+
+    this.todoTitle = ''
+    this.idForTodo++;
+  }
+
+  deleteTodo(id: number): void {
+    this.todos = this.todos.filter(todo => todo.id !== id)
+  }
+}
